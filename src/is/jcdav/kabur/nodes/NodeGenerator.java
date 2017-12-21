9dcb35bbeb6818e193ed09463786e6ec1f269ead
Jackson Davis <jackson@jcdav.is>
2017-12-20 22:23:48 -0800

    Prefer known interesting constants (0, +/-1, min/max value)

--- src/is/jcdav/kabur/nodes/NodeGenerator.java
+++ src/is/jcdav/kabur/nodes/NodeGenerator.java
@@ -9,14 +9,24 @@ public class NodeGenerator {
 
     public static Node generate(Random r, int level) {
         if (r.nextInt(100) < level*5) {
-            if (r.nextInt(100) < 60)
-                return Node.constant(r.nextInt());
-            else if (r.nextBoolean())
+            if (r.nextInt(100) < 60) {
+                if (r.nextBoolean()) {
+                    switch (r.nextInt(5)) {
+                        case 0: return Node.constant(Integer.MIN_VALUE);
+                        case 1: return Node.constant(-1);
+                        case 2: return Node.constant(0);
+                        case 3: return Node.constant(1);
+                        case 4: return Node.constant(Integer.MAX_VALUE);
+                    }
+                } else {
+                    return Node.constant(r.nextInt());
+                }
+            } else if (r.nextBoolean())
                 return Node.leftParam();
             else
                 return Node.rightParam();
         }
-        //Arbitary
+        //These numbers are extremely arbitrary
         if (r.nextInt(100) < 10) {
             // To make conditionals more meaningful, prefer simpler expressions and always check param
             Node param = r.nextBoolean()? Node.leftParam() : Node.rightParam();
